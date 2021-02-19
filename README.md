f562af4ea33b77a6b905d790a42bffec2cf0d2c9
mame-baba <mamebabambaye@gmail.com>
2021-02-19 22:51:18 +0000

    Level 100, 200

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ The requirements for the project are here:
 
 https://github.com/CodeYourFuture/syllabus/tree/master/js-core-3/tv-show-dom-project/readme.md
 
+
 ## Solution
 
-The solution to this project will be given to you after you have completed it. You will be able to find the solition here
+The solution to this project will be given to you after you have completed it. You will be able to find the solution here
 
 https://github.com/CodeYourFuture/tv-show-dom-project-solution
--- script.js
+++ script.js
@@ -1,12 +1,90 @@
+const allEpisodes = getAllEpisodes();
+makePageForEpisodes(allEpisodes);
+let root = document.getElementById("root");
+let search = document.getElementById("search");
+
+let input = document.getElementById("myInput");
+input.type = "text";
+input.id = "myInput";
+input.onkeyup = "mySearchFunction()";
+input.placeholder = "Search";
+
+
+
+// setting number zero in front
+function pad(num, size) {
+  num = num.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
+}
+
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  let root = document.getElementById("root");
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  const root = document.getElementById("root");
+  allEpisodes.forEach((elem) => {
+    let list = document.createElement("div");
+    list.classList.add("container-list");
+    let title = document.createElement("h2"); //  title
 
-window.onload = setup;
+    let img = document.createElement("img"); //  image
+
+    let summary = document.createElement("p"); //  summary
+    title.id = "h2";
+    img.id = "img";
+    summary.id = "p";
+    title.innerText = `${elem.name} - S${pad(elem.season, 2)}E${pad(
+      elem.number,
+      2
+    )}`;
+
+    img.src = `${elem.image.medium}`;
+    summary.innerHTML = elem.summary;
+    list.appendChild(title);
+    list.appendChild(img);
+    list.appendChild(summary);
+    root.appendChild(list);
+  });
+}
+// getAllEpisodes();
+function mySearchFunction() {
+  // Declare variables
+  var input, filter, ul, li, item, i, txtValue;
+  //   // User Input
+  input = document.getElementById("myInput");
+  console.log("input",input);
+  //   // Filter, makes search not case sensitive
+  filter = input.value.toUpperCase();
+  console.log("filter",filter);
+  //   // Grabs the parent element by id
+  ul = document.getElementsById("root");
+  //   // Individual item on list
+  li = ul.getElementByTagName("div");
+
+  //   // Treats lists items like an array, where each item can be accessed through it's index
+  for (i = 0; i < allEpisodes.length; i++) {
+    item = allEpisodes[i].name;
+    //    // Iterate over each list episode to see if the value of the input, ignoring case, matches the inner text or inner html of the item.
+
+    if (item.toUpperCase().indexOf(filter) > -1) {
+      //     // Displays list items that are a match, and nothing if no match
+      ul[i].style.display = "";
+    } else {
+      ul[i].style.display = "none";
+    }
+  }
+}
+  //   input.addEventListener("click", mySearchFunction);
+
+  // mySearchFunction();
+  // input.addEventListener('keyup', (event) => {
+  //   var search = event.target.value
+  //   }
+
+
+  window.onload = setup;
