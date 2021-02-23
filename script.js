a838fdd9ffe35da9fa5db1726e5c476cba63247e
mame-baba <mamebabambaye@gmail.com>
2021-02-23 23:00:58 +0000

    script.js update

--- script.js
+++ script.js
@@ -3,14 +3,6 @@ makePageForEpisodes(allEpisodes);
 let root = document.getElementById("root");
 let search = document.getElementById("search");
 
-let input = document.getElementById("myInput");
-input.type = "text";
-input.id = "myInput";
-input.onkeyup = "mySearchFunction()";
-input.placeholder = "Search";
-
-
-
 // setting number zero in front
 function pad(num, size) {
   num = num.toString();
@@ -20,16 +12,20 @@ function pad(num, size) {
 
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  // const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
   let root = document.getElementById("root");
 }
 
-function makePageForEpisodes(episodeList) {
+function makePageForEpisodes(filteredEpisodes) {
   const root = document.getElementById("root");
-  allEpisodes.forEach((elem) => {
+
+  for (var i = 0; i < allEpisodes.length; i++) {
     let list = document.createElement("div");
-    list.classList.add("container-list");
+
+    list.value = i;
+    list.className = "episode";
+    // console.log(list.value);
     let title = document.createElement("h2"); //  title
 
     let img = document.createElement("img"); //  image
@@ -38,53 +34,57 @@ function makePageForEpisodes(episodeList) {
     title.id = "h2";
     img.id = "img";
     summary.id = "p";
-    title.innerText = `${elem.name} - S${pad(elem.season, 2)}E${pad(
-      elem.number,
+    title.innerText = `${allEpisodes[i].name} - S${pad(
+      allEpisodes[i].season,
       2
-    )}`;
+    )}E${pad(allEpisodes[i].number, 2)}`;
+    img.src = `${allEpisodes[i].image.medium}`;
+    summary.innerHTML = allEpisodes[i].summary;
 
-    img.src = `${elem.image.medium}`;
-    summary.innerHTML = elem.summary;
     list.appendChild(title);
+
     list.appendChild(img);
+
     list.appendChild(summary);
+
     root.appendChild(list);
-  });
+  }
 }
 // getAllEpisodes();
 function mySearchFunction() {
   // Declare variables
-  var input, filter, ul, li, item, i, txtValue;
+  var input, filter, item, i;
+
+  let filteredEpisodes = [];
   //   // User Input
   input = document.getElementById("myInput");
-  console.log("input",input);
+  console.log(input);
+
   //   // Filter, makes search not case sensitive
   filter = input.value.toUpperCase();
-  console.log("filter",filter);
-  //   // Grabs the parent element by id
-  ul = document.getElementsById("root");
-  //   // Individual item on list
-  li = ul.getElementByTagName("div");
 
-  //   // Treats lists items like an array, where each item can be accessed through it's index
+  console.log("filter", filter);
+  //   // Grabs the parent element by id
+  root = document.getElementById("root");
+
+  //   // Individual item on list
+  episodes = document.getElementsByClassName("episode");
+
+ root.innerHTML = "";
+
+  // Treats lists items like an array, where each item can be accessed through it's index
   for (i = 0; i < allEpisodes.length; i++) {
     item = allEpisodes[i].name;
-    //    // Iterate over each list episode to see if the value of the input, ignoring case, matches the inner text or inner html of the item.
+    // Iterate over each list episode to see if the value of the input, ignoring case, matches the inner text or inner html of the item.
 
     if (item.toUpperCase().indexOf(filter) > -1) {
-      //     // Displays list items that are a match, and nothing if no match
-      ul[i].style.display = "";
-    } else {
-      ul[i].style.display = "none";
+      // Displays list items that are a match, and nothing if no match
+      // filteredEpisodes.push(episodes[i])  ;
+
+      episodes[i].style.display = "none";
     }
   }
 }
-  //   input.addEventListener("click", mySearchFunction);
 
-  // mySearchFunction();
-  // input.addEventListener('keyup', (event) => {
-  //   var search = event.target.value
-  //   }
+window.onload = setup;
 
-
-  window.onload = setup;
